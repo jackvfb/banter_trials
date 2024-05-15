063ebfffd927700865d354e4f2536a5310715098
Jackson VFB <jackvfb@gmail.com>
2024-05-14 17:30:10 -0700

    Update makeStudy.R

--- R/makeStudy.R
+++ R/makeStudy.R
@@ -3,7 +3,7 @@ library(PAMpal)
 makeStudies <- function(dir){
   dbs <- file.path(dir, "db")
   bins <- file.path(dir, "binaries")
-  pps <- PAMpalSettings(db = dbs, binaries = bins, sr_hz = "auto", filterfrom_khz = 100, filterto_khz = 160, winLen_sec = 0.0025)
+  pps <- PAMpalSettings(db = dbs, binaries = bins, sr_hz = 384000, filterfrom_khz = 100, filterto_khz = 160, winLen_sec = 0.0025)
   study <- processPgDetections(pps = pps, mode = "db", id = basename(dir))
   study <- setSpecies(study, method = "pamguard")
   return(study)
